208dc93c0c54dbc249094f4d4b0f669235f6151b
mandar kamble <mandarkamble2002@gmail.com>
2022-09-06 19:19:17 +0530

    DESCRIBE COMMIT IN A FEW WORDS

---
+++ app.js
@@ -0,0 +1,90 @@
+require('dotenv').config()
+
+const bodyParser = require("body-parser");
+const express =require("express");
+const ejs=require("ejs");
+const { default: mongoose } = require("mongoose");
+// const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app=express();
+
+// console.log(process.env.API_kEY);
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+// 1
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+// 2
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+// 3
+const User=new mongoose.model("User",userSchema);
+
+//
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+//
+app.post("/register",(req,res)=>{
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username},(err,founduser)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(founduser){
+                if(founduser.password===password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+    
+});
+
+app.get("/logout",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/submit",(req,res)=>{
+    res.render("submit");
+});
+
+
+app.listen(3000,()=>{
+    console.log("App lsitening on Port 3000");
+});
